060b34f288b60026caeb0588cd943b77ada5c619
gnobisP <gustavopimenta90@gmail.com>
2025-01-25 07:46:15 -0300

    FinalizadoFormatacao

--- automacoes/cenario2.py
+++ automacoes/cenario2.py
@@ -8,12 +8,14 @@ Quando o usuário clicar na opção para alterar a senha e inserir um email vál
 Então, a página deve exibir uma mensagem informando que um e-mail foi enviado para a mudança de senha 
 """
 import time
+import sys
 
-
-from uteis.exceptionsLogin import InvalidPasswordException, InvalidEmailException, AuthenticationFailedException
+# Imports internos
+from uteis.exceptionsLogin import (LoginInvalidEmailException)
 from uteis.moduloLogin import alteraSenhaUsuario
 from uteis.moduloConfiguracoes import inicia_servico
 
+# Classes internas
 from classes.Usuario import Usuario, Date
 
 if __name__ == "__main__":
@@ -26,13 +28,20 @@ if __name__ == "__main__":
         password="12345678",
         date_of_birth=Date("15", "1", "1990")
     )
-    navegador = inicia_servico()
+    navegador = inicia_servico() #inicia o selenium na página inicial
 
+    #Altera a senha de um usuario
     try:
         alteraSenhaUsuario(navegador, usuario)
         print("Email para mudança de senha enviado!")
+
+    except LoginInvalidEmailException as e2:
+        print(f"Erro: {e2}")
+        
     except Exception as e:
-        print(f"Erro inesperado: {e}")
+        print(f"Erro Inesperado: {e}")
+        navegador.quit()
+        sys.exit(-1)
 
     time.sleep(7)
     navegador.quit()
