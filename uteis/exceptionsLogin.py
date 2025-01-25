b0fff4913fab1e03b47eacce54af4ad01f719d09
gnobisP <gustavopimenta90@gmail.com>
2025-01-25 07:47:06 -0300

    FinalizadoFormatacao

--- uteis/exceptionsLogin.py
+++ uteis/exceptionsLogin.py
@@ -1,11 +1,20 @@
-class InvalidEmailException(Exception):
+#Excessao para email invalido(não segue o padrao nome@email.com)
+class LoginInvalidEmailException(Exception):
     """Exceção para e-mail inválido."""
-    pass
+    def __init__(self, message="O e-mail fornecido é inválido."):
+        self.message = message
+        super().__init__(self.message)
 
-class InvalidPasswordException(Exception):
+# Exceção para senha invalida (menos de 6 caracteres)
+class LoginInvalidPasswordException(Exception):
     """Exceção para senha inválida."""
-    pass
+    def __init__(self, message="A senha fornecida é inválida."):
+        self.message = message
+        super().__init__(self.message)
 
-class AuthenticationFailedException(Exception):
-    """Exceção para credenciais inválida."""
-    pass
+# Exceção para Senha ou email errado
+class LoginAuthenticationFailedException(Exception):
+    """Exceção para credenciais inválidas."""
+    def __init__(self, message="Email ou senha incorretos."):
+        self.message = message
+        super().__init__(self.message)
