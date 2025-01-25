59eac5249f14b8f999f114678ab9457e46bc3f90
gnobisP <gustavopimenta90@gmail.com>
2025-01-25 07:59:49 -0300

    FinalizadoFormatacao

--- uteis/moduloLogin.py
+++ uteis/moduloLogin.py
@@ -1,34 +1,33 @@
 import re
-import time
 
+# Imports internos
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.ui import Select
 
-
-from uteis.exceptionsLogin import InvalidPasswordException, InvalidEmailException, AuthenticationFailedException
-from uteis.exceptionsCadastro import LastNameRequiredException, FirstNameRequiredException, PasswordRequiredException, InvalidDateOfBirthException, AuthenticationFailedException 
-
+# Imports Exception
+from uteis.exceptionsLogin import LoginInvalidEmailException, LoginInvalidPasswordException, LoginAuthenticationFailedException
+from uteis.exceptionsCadastro import CadastroLastNameRequiredException, CadastroFirstNameRequiredException, CadastroPasswordRequiredException, CadastroInvalidDateOfBirthException, CadastroAuthenticationFailedException
 
 def realiza_login(navegador, usuario):
     """Realiza login no site."""
     navegador.get('http://www.automationpractice.pl/index.php?controller=authentication&back=my-account')
     
     # Preenche o campo de e-mail
-    email_field = WebDriverWait(navegador, 5).until(  # Espera reduzida para 5 segundos
+    email_field = WebDriverWait(navegador, 5).until(  
         EC.visibility_of_element_located((By.XPATH, '//*[@id="email"]'))
     )
     email_field.send_keys(usuario.email)
     
     # Preenche o campo de senha
-    password_field = WebDriverWait(navegador, 5).until(  # Espera reduzida para 5 segundos
+    password_field = WebDriverWait(navegador, 5).until(  
         EC.visibility_of_element_located((By.XPATH, '//*[@id="passwd"]'))
     )
     password_field.send_keys(usuario.password)
     
     # Clica no botão de login
-    login_button = WebDriverWait(navegador, 5).until(  # Espera reduzida para 5 segundos
+    login_button = WebDriverWait(navegador, 5).until(  
         EC.element_to_be_clickable((By.XPATH, '//*[@id="SubmitLogin"]/span'))
     )
     login_button.click()
@@ -40,35 +39,38 @@ def realiza_login(navegador, usuario):
             EC.url_to_be('http://www.automationpractice.pl/index.php?controller=my-account')
         )
     except Exception as e:
-        #TODO otimiza em tempo
         # Se login falhou, captura o erro
         try:
-            captura_erros_login(navegador)
+            captura_erros_login(navegador) #captura todas excessoes possiveis de login
         except Exception as e:
             raise
 
 def captura_erros_login(navegador):
     """Captura mensagens de erro exibidas na página e lança exceções conforme o erro."""
-    # Aumentando o tempo de espera para garantir que o erro seja capturado
+    
+    #Acessao o alertDanger
     erro_elemento = WebDriverWait(navegador, 10).until(
         EC.visibility_of_element_located((By.CLASS_NAME, 'alert-danger'))
     )
     
-    # Extrai o texto do erro
+    # Extrai o texto do alertDanger
     mensagem_erro = erro_elemento.text
+
     # Remove quebras de linha e espaços extras
     mensagem_erro = re.sub(r'\s+', ' ', mensagem_erro).strip()
     
     # Verifica o tipo de erro e lança a exceção correspondente
     if "Invalid email address." in mensagem_erro:
-        raise InvalidEmailException("O endereço de e-mail fornecido é inválido.")
+        raise LoginInvalidEmailException()
+    
     elif "Invalid password." in mensagem_erro:
-        raise InvalidPasswordException("A senha fornecida é inválida.")
+        raise LoginInvalidPasswordException()
+    
     elif "Authentication failed." in mensagem_erro:
-        raise AuthenticationFailedException("Email ou senha incorretos.")
+        raise LoginAuthenticationFailedException()
+    
     else:
-        raise RuntimeError(f"Erro desconhecido: {mensagem_erro}")  # Lançando exceção para erro desconhecido
-
+        raise Exception  # Lançando exceção para erro desconhecido
 
 def captura_erros_cadastro(navegador):
     """Captura mensagens de erro exibidas na página e lança exceções conforme o erro."""
@@ -81,7 +83,7 @@ def captura_erros_cadastro(navegador):
         # Captura todas as mensagens dentro da lista <ol>
         error_list = alert_box.find_elements(By.TAG_NAME, 'li')
 
-        error_message = "" 
+        error_message = ""
         for index, error in enumerate(error_list, start=1):
             error_message += f"Erro {index}: {error.text.strip().lower()} "  # Concatenar com um separador claro, como um espaço ou uma quebra de linha
 
@@ -90,24 +92,25 @@ def captura_erros_cadastro(navegador):
     finally:
         # Lançando exceções com base nas mensagens de erro
         if "lastname is required" in error_message:
-            raise LastNameRequiredException("Last name is required.")
+            raise CadastroLastNameRequiredException()
+        
         elif "firstname is required" in error_message:
-            raise FirstNameRequiredException("First name is required.")
+            raise CadastroFirstNameRequiredException()
+        
         elif "passwd is required" in error_message:
-            raise PasswordRequiredException("Password is required.")
+            raise CadastroPasswordRequiredException()
+        
         elif "invalid date of birth" in error_message:
-            raise InvalidDateOfBirthException("Invalid date of birth.")
+            raise CadastroInvalidDateOfBirthException()
+        
         elif "authentication failed" in error_message or "invalid credentials" in error_message:
-            raise AuthenticationFailedException("Authentication failed.")
+            raise CadastroAuthenticationFailedException()
 
 def cadastrarUsuario(navegador, usuario):
     """Cadastra usuario no site."""
     navegador.get('http://www.automationpractice.pl/index.php?controller=authentication&back=my-account')
     
     try:
-        # Acessar a página de cadastro (redundante, pois já foi acessada acima)
-        # navegador.get('http://www.automationpractice.pl/index.php?controller=authentication&back=my-account')
-
         # Preencher o campo de email
         email_field = WebDriverWait(navegador, 20).until(
             EC.visibility_of_element_located((By.ID, 'email_create'))
@@ -120,8 +123,12 @@ def cadastrarUsuario(navegador, usuario):
         )
         create_account_button.click()
 
+    except Exception as e:
+        raise e
+
+    try:
         # Aguarde o carregamento do elemento "days", elemento qualquer, o importante é a página carregar
-        day_dropdown = WebDriverWait(navegador, 20).until(
+        day_dropdown = WebDriverWait(navegador, 5).until(
             EC.presence_of_element_located((By.ID, 'days'))
         )
 
@@ -143,13 +150,14 @@ def cadastrarUsuario(navegador, usuario):
             EC.url_to_be('http://www.automationpractice.pl/index.php?controller=my-account')
         )
     except Exception as e:
-        # Se o login falhou, captura o erro
+        # Se o login falhou, captura os possiveis erros e lança as excessoes
         try:
             captura_erros_cadastro(navegador)
         except Exception as e:
             raise e        
    
 def preencheFormulario(navegador, usuario):
+    """Preenche todo o fromulario de cadastro com javascript"""
     navegador.execute_script("""
         // Selecionar o radio button (Mr/Mrs)
         if(arguments[0] === 'Mr' || arguments[0] === 'Mrs')
@@ -196,4 +204,57 @@ def preencheFormulario(navegador, usuario):
         usuario.password, 
         usuario.date_of_birth.day, 
         usuario.date_of_birth.month, 
-        usuario.date_of_birth.year)  # Ano de nascimento    
+        usuario.date_of_birth.year)
+
+def alteraSenhaUsuario(navegador, usuario):
+    """Muda senha no site."""
+    navegador.get('http://www.automationpractice.pl/index.php?controller=authentication&back=my-account')
+
+    #Clica no botão "esqueci minha senha"
+    try:
+        forgotKeyWord = WebDriverWait(navegador, 10).until(
+        EC.element_to_be_clickable((By.XPATH, '//*[@id="login_form"]/div/p[1]/a'))
+    )
+        forgotKeyWord.click()
+    except Exception as e:
+        raise e
+    
+    try:
+        email_input = WebDriverWait(navegador, 10).until(
+        EC.visibility_of_element_located((By.XPATH, '//*[@id="email"]'))
+    )
+        email_input.send_keys(usuario.email)  # Preenchendo com um e-mail de teste
+
+    # Localize o botão de submit e clique nele
+        submit_button = WebDriverWait(navegador, 10).until(
+        EC.element_to_be_clickable((By.XPATH, '//*[@id="form_forgotpassword"]/fieldset/p/button'))
+        )
+        submit_button.click()
+    except Exception as e:
+        raise e
+    
+    emailEnviado = False
+    try:
+        # Esperando até que o elemento esteja visível na página
+        elemento = WebDriverWait(navegador, 10).until(
+            EC.visibility_of_element_located((By.XPATH, '//*[@id="center_column"]/div/p'))
+        )
+        
+        texto = elemento.text
+
+        # Verificando se o texto contém "A confirmation email"
+        if "A confirmation email" in texto:
+            emailEnviado = True
+
+        else:
+            emailEnviado = False
+
+    except Exception as e:
+        raise e
+    finally:
+        if(emailEnviado == False):
+            raise LoginInvalidEmailException()
+
+    
+
+    
