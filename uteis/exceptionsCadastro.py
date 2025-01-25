54c33f6ecda7a22caae576a0a92d32607ac5955b
gnobisP <gustavopimenta90@gmail.com>
2025-01-25 07:46:50 -0300

    FinalizadoFormatacao

--- uteis/exceptionsCadastro.py
+++ uteis/exceptionsCadastro.py
@@ -1,24 +1,41 @@
 # Exceção para quando o sobrenome não é fornecido
-class LastNameRequiredException(Exception):
-    """Exceção para sobrenome obrigatório.""" 
-    pass  
+class CadastroLastNameRequiredException(Exception):
+    """Exceção para sobrenome obrigatório."""
+    def __init__(self, message="O sobrenome é obrigatório."):
+        self.message = message
+        super().__init__(self.message)
 
-class FirstNameRequiredException(Exception):
-    """Exceção para primeiro nome obrigatório.""" 
-    pass 
+# Exceção para quando o primeiro nome não é fornecido
+class CadastroFirstNameRequiredException(Exception):
+    """Exceção para primeiro nome obrigatório."""
+    def __init__(self, message="O primeiro nome é obrigatório."):
+        self.message = message
+        super().__init__(self.message)
 
-class PasswordRequiredException(Exception):
-    """Exceção para senha obrigatória."""  
-    pass 
+# Exceção para quando a senha não é fornecida
+class CadastroPasswordRequiredException(Exception):
+    """Exceção para senha obrigatória."""
+    def __init__(self, message="A senha é obrigatória."):
+        self.message = message
+        super().__init__(self.message)
 
-class InvalidDateOfBirthException(Exception):
-    """Exceção para data de nascimento inválida.""" 
-    pass  
+# Exceção para data de nascimento inválida (Não preencheu todos campos)
+class CadastroInvalidDateOfBirthException(Exception):
+    """Exceção para data de nascimento inválida."""
+    def __init__(self, message="Data de nascimento inválida."):
+        self.message = message
+        super().__init__(self.message)
 
-class AuthenticationFailedException(Exception):
-    """Exceção para credenciais inválidas.""" 
-    pass  
+# Exceção para credenciais inválidas no cadastro
+class CadastroAuthenticationFailedException(Exception):
+    """Exceção para credenciais inválidas."""
+    def __init__(self, message="Credenciais inválidas (email ou senha)."):
+        self.message = message
+        super().__init__(self.message)
 
-class emailEddressAlreadyRegistered(Exception):
-    """Uma conta com esse email já existe""" 
-    pass  
+# Exceção para quando o e-mail já está registrado
+class CadastroEmailAddressAlreadyRegistered(Exception):
+    """Uma conta com esse email já existe."""
+    def __init__(self, message="Uma conta com esse e-mail já está registrada."):
+        self.message = message
+        super().__init__(self.message)
